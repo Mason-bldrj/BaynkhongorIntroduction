56f63f385a526153f7d3b22b1f674dc012cd6987
Batuusch <lbanzragch115@gmail.com>
2024-08-29 23:10:48 +0800

    homepage1stpush7

--- front-end/src/app/components/big/eventArea.tsx
+++ front-end/src/app/components/big/eventArea.tsx
@@ -1,7 +1,23 @@
-'use client'
+"use client";
 import { OrangeBourd } from "../detail/orengeBourd";
 import { EventCard } from "../detail/evenCard";
+import { ArrowButtons2 } from "../detail/arrowButtons";
+import { useState } from "react";
+import { bplace } from "@/app/data";
 export const EventArea = () => {
+  const [startIndex, setStartIndex] = useState(0);
+  const visibleCount = 1;
+  const handleNext = () => {
+    if (startIndex + visibleCount < bplace.length) {
+      setStartIndex(startIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (startIndex > 0) {
+      setStartIndex(startIndex - 1);
+    }
+  };
   return (
     <div className="w-full mt-10">
       <div className="flex w-full justify-between">
@@ -10,9 +26,9 @@ export const EventArea = () => {
           <OrangeBourd data={"ЭВЕНТ , АРГА ХЭМЖЭЭ"} />
         </div>
       </div>
-      <div className="flex justify-between">
-          <EventCard/>
-         
+      <div className="flex justify-between h-full items-center">
+        <EventCard bplace={bplace} startIndex={startIndex} />
+        <ArrowButtons2 handleNext={handleNext} handlePrev={handlePrev} />
       </div>
     </div>
   );
--- front-end/src/app/components/detail/evenCard.tsx
+++ front-end/src/app/components/detail/evenCard.tsx
@@ -1,62 +1,37 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { bplace } from "@/app/data";
 import { useState, useEffect } from "react";
 import { ArrowButtons2 } from "../detail/arrowButtons";
 
-export const EventCard = () => {
-  const [sum, setSum] =useState(0);
-  const [isSliding, setIsSliding] = useState(false);
-  useEffect(() => {
-    setSum(0)
-    setIsSliding(false)
-  }, [])
-  const handleNext = () => {
-    setIsSliding(true);
-    setTimeout(() => {
-      if (sum > bplace.length - 2) {
-        setSum(0);
-      } else {
-        setSum(sum + 1);
-      }
-      setIsSliding(false);
-    }, 500);
-  };
-  const handlePrev = () => {
-    setIsSliding(true);
-
-    setTimeout(() => {
-      if (sum >= 1) {
-        setSum(sum - 1);
-      } else {
-        setSum(bplace.length - 1);
-      }
-      setIsSliding(false);
-    }, 500);
-  };
-
+export const EventCard = ({ bplace, startIndex }: any) => {
   return (
-    <div className="mt-5 w-full">
-      <div className="w-full h-[478px] flex justify-between items-center relative overflow-hidden">
+    <div className="w-full h-[470px] flex ">
+      <div className="flex w-[1155px] h-[470px] overflow-hidden flexxx transition-transform duration-300 justify-start">
         <div
-          className={`  relative w-full h-[fit] flex justify-between items-center transition-transform duration-500 ${
-            isSliding ? "transform -translate-y-full" : "transform translate-0"
-          }`}
+          className="flexxx transition-transform duration-300 justify-start gap-[50px]"
+          style={{
+            transform: `translateY(-${startIndex * 490}px)`,
+          }}
         >
-          <Image
-            key={sum}
-            className="max-w-[1024px] max-h-[420px] shadow-[30px_30px] shadow-[#f3f3f3] rounded-md transition-transform duration-500 ease-in-out"
-            src={bplace[sum].icon}
-            width={1000}
-            height={478}
-            alt="Carousel image"
-          />
-          <div className="w-[910px] h-[112px] flex flex-col items-center justify-around absolute opacity-75 bottom-5 left-10 rounded-md text-white bg-black">
-            <div>{bplace[sum].title}</div>
-            <div>{bplace[sum].idk}</div>
-          </div>
+          {bplace.map((el: any, i: number) => {
+            return (
+              <div key={i} className="w-[1000px] h-[440px] relative">
+                <Image
+                  className=" w-[1000px] max-h-[440px] shadow-[30px_30px] rounded-md shadow-gray-300"
+                  src={el.icon}
+                  width={1000}
+                  height={440}
+                  alt="Carousel image"
+                />
+                <div className="w-[910px] h-[112px] flex flex-col justify-around *:px-5 rounded-md  items-center bg-black opacity-75 text-white absolute left-[50px] bottom-[20px] overflow-hidden">
+                  <div>{el.title}</div>
+                  <div>{el.idk}</div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <ArrowButtons2 handleNext={handleNext} handlePrev={handlePrev} />
       </div>
     </div>
   );

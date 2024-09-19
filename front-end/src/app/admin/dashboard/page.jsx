25ec775d9234ed01e5ef1e080ceb8ff4b3f0ef8e
Batuusch <lbanzragch115@gmail.com>
2024-09-19 11:57:39 +0800

    main shuud

--- front-end/src/app/admin/dashboard/page.jsx
+++ front-end/src/app/admin/dashboard/page.jsx
@@ -4,7 +4,7 @@ import AdminMain from "@/app/components/admin/AdminMains";
 import AdminSideBar from "@/app/components/admin/AdminSideBar";
 import DashboardSec from "@/app/components/admin/Dashboard";
 import { useRouter } from "next/navigation";
-
+import { CiLogout } from "react-icons/ci";
 import { useEffect, useState } from "react";
 
 const AdminDashboard = () => {
@@ -12,6 +12,7 @@ const AdminDashboard = () => {
   const [admin, setAdmin] = useState("");
   const [changedRequest, setChangedRequest] = useState("CREATE");
   const [menus, setMenu] = useState("Бидний тухай");
+  const [checkDisplay, setCheckDisplay] = useState(Boolean);
   const e = async () => {
     const inadmin = await localStorage.getItem("admin");
 
@@ -21,30 +22,55 @@ const AdminDashboard = () => {
       }
     }, 1000);
   };
+  const logOut = () => {
+    localStorage.clear();
+  };
   useEffect(() => {
     e();
   }, [admin]);
+  useEffect(() => {
+    if (window.innerWidth > 640) {
+      setCheckDisplay(true);
+    } else {
+      setCheckDisplay(false);
+      alert("Та зөвхөн том дэлгэцэн дээр мэлээлэл оруулах боломжтой.");
+      router.push("/");
+    }
+  }, []);
   return (
     <div className="flex ">
-      <AdminSideBar
-        setMenu={setMenu}
-        changedRequest={changedRequest}
-        setChangedRequest={setChangedRequest}
-      />
-      <div
-        className={` p-[50px] w-[70%]  justify-center ${
-          changedRequest === "CREATE" ? "flex" : "hidden"
-        } `}
-      >
-        <AdminMain menus={menus}></AdminMain>
-      </div>
-      <div
-        className={` p-[50px] w-full    ${
-          changedRequest === "CREATE" ? "hidden" : "flex"
-        } `}
-      >
-        <DashboardSec menus={menus}></DashboardSec>
-      </div>
+      {checkDisplay == true ? (
+        <div className="flex relative">
+          <button
+            onClick={logOut}
+            className=" absolute top-1 right-1 flex items-center hover:scale-[110%] hover:bg-slate-100 gap-2 border rounded-md p-2"
+          >
+            <CiLogout />
+            <div>Гарах</div>
+          </button>{" "}
+          <AdminSideBar
+            setMenu={setMenu}
+            changedRequest={changedRequest}
+            setChangedRequest={setChangedRequest}
+          />
+          <div
+            className={` p-[50px] w-full  justify-center ${
+              changedRequest === "CREATE" ? "flex" : "hidden"
+            } `}
+          >
+            <AdminMain menus={menus}></AdminMain>
+          </div>
+          <div
+            className={` p-[50px] w-full    ${
+              changedRequest === "CREATE" ? "hidden" : "flex"
+            } `}
+          >
+            <DashboardSec menus={menus}></DashboardSec>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
